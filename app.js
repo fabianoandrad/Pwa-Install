364faa826f05ec5b261eaa2026b0dad8e89da701
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-08-13 12:38:13 -0300

    chore: teste com ios

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ window.addEventListener("beforeinstallprompt", (event) => {
   installButton.removeAttribute("hidden");
 });
 
+window.addEventListener('load', () => {
+  if (shouldShowInstallBanner()) {
+    document.getElementById('ios-install-banner').style.display = 'block';
+  }
+});
+
+function shouldShowInstallBanner() {
+  const isIOS = /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
+  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  const isInStandalone = window.navigator.standalone === true;
+
+  return isIOS && isSafari && !isInStandalone;
+}
+
+
 installButton.addEventListener("click", async () => {
   if (!installPrompt) {
     return;
